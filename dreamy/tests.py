5aa0c1bb97a0cdff0ad31d3a87dd68de0d76c5f8
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-09-07 14:54:23 +0200

    Added user creation test

--- dreamy/tests.py
+++ dreamy/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserCreateTest(TestCase):
+    """User creation-related tests"""
+
+    def setUp(self):
+        """Set up the user for testing"""
+        User.objects.create(username='foo', password='bartestpass')
+
+    def test_user_exists(self):
+        """Test the existence of the user created in SetUp"""
+        user = User.objects.get(username='foo')
+        self.assertIsNotNone(user.username, 'foo')
