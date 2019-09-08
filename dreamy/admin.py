74f3b6dfaa031bf66de2f7d6877fafa5cfcc4441
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-09-08 13:15:43 +0200

    Added models and registered them in admin.
    3 models were added: User (extending the default auth.User),
    UserFollower (link table for modeling followers), and Post (for modeling posts)

--- dreamy/admin.py
+++ dreamy/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserFollower, Post
+from .forms import DreamyUserCreationForm, DreamyUserChangeForm
 
 # Register your models here.
+
+
+class DreamyUserAdmin(UserAdmin):
+    """This class overrides the default UserAdmin to reflect the overridden User model"""
+
+    add_form = DreamyUserCreationForm
+    form = DreamyUserChangeForm
+    model = User
+
+
+admin.site.register(User)
+admin.site.register(UserFollower)
+admin.site.register(Post)
--- dreamy/models.py
+++ dreamy/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """Overridden abstract User class to add bio, birth date and avatar fields"""
+
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True)
+
+
+class Post(models.Model):
+    """ORM model for a Dreamy post"""
+
+    description = models.TextField(max_length=300)
+    image = models.ImageField(upload_to='images/')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    date = models.DateField()
+
+    def __repr__(self):
+        """Custom string representation"""
+        return f"<Post: {self.description[:50]}>"
+
+
+class UserFollower(models.Model):
+    """Link table which links a user and their followers"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='users')
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                 related_name='followers')
