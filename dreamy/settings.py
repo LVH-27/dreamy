40c3ea7dc3eadd023b0dc9100fa162d7b517e9c5
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-09-08 13:17:34 +0200

    Added custom user model and media paths to settings

--- dreamy/settings.py
+++ dreamy/settings.py
@@ -120,7 +120,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+# User management
+
+AUTH_USER_MODEL = 'dreamy.User'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Media files configuration - used for uploading images
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
