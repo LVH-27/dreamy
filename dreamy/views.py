0016439e78de7f6b687a580db11f467073c702e3
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-09-08 13:22:06 +0200

    Added user viewing and browsing, and custom forms.
    User viewing now accepts user_id. User browsing is currently broken (working on it).

--- dreamy/views.py
+++ dreamy/views.py
@@ -1,40 +1,15 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, get_user_model
 from django.shortcuts import render, redirect
-from . import PRETTY_APP_NAME
+from django.core.paginator import Paginator
 
 from os.path import join
+
+from .forms import DreamyUserCreationForm
+from . import PRETTY_APP_NAME, models
+
 # Create your views here.
 
 APP_NAME = 'dreamy'
-
-
-mock_posts = [
-    {'id': 1,
-     'author': 'rincewind',
-     'date': '21.02.2014.',
-     'description': 'This is test photo',
-     'image': join('images', 'rincewind.png')
-     },
-    {'id': 2,
-     'author': 'rincewind',
-     'date': '21.03.2014.',
-     'description': 'This is another test photo',
-     'image': join('images', 'rincewind2.png')
-     },
-    {'id': 3,
-     'author': 'rincewind',
-     'date': '21.04.2014.',
-     'description': 'This is yet another test photo',
-     'image': join('images', 'rincewind3.png')
-     },
-    {'id': 4,
-     'author': 'roncewind',
-     'date': '21.04.2014.',
-     'description': 'This is not mine',
-     'image': join('images', 'roncewind.png')
-     },
-]
 
 
 def home(request):
@@ -45,7 +20,7 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = DreamyUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -54,22 +29,41 @@ def register(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = DreamyUserCreationForm()
     return render(request,
                   join(APP_NAME, 'register.html'),
                   {'form': form, 'PRETTY_APP_NAME': PRETTY_APP_NAME})
 
 
-def profile(request, user_id):
-    user_posts = []
-    for post in mock_posts:
-        if post['author'] == 'rincewind':
-            user_posts.append(post)
+def profile(request, user_id=None):
+    if user_id is None:
+        user_id = request.user.id
+    user = get_user_model().objects.get(id=user_id)
+    user_posts = models.Post.objects.filter(author=user)
+
     return render(request,
                   join(APP_NAME, 'profile.html'),
-                  {'user_id': user_id,
+                  {'user': user,
                    'PRETTY_APP_NAME': PRETTY_APP_NAME,
                    'user_posts': user_posts})
+
+
+def browse_users(request, follow=None):
+    if follow is None:
+        user_list = models.User.objects.all()
+    # elif follow == 'followers':
+    #     user_list = models.UserFollower.objects.filter(TODO)
+    #     pass
+    # elif follow == 'following':
+    #     user_list = models.UserFollower.objects.filter(TODO)
+    paginator = Paginator(user_list, 20)
+
+    page = request.GET.get('page')
+    users = paginator.get(page)
+    return render(request,
+                  join(APP_NAME, 'browse_users.html'),
+                  {'users': users,
+                   'PRETTY_APP_NAME': PRETTY_APP_NAME})
 
 
 def post(request, post_id):
