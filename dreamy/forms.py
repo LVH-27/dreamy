8dd9e3b8b17dcb49ad5c3f1bd744873323d1716f
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-09-11 20:39:51 +0200

    Added Post creation form

--- dreamy/forms.py
+++ dreamy/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import ModelForm, ModelChoiceField, DateTimeField, HiddenInput
+from .models import Post
 
 
 User = get_user_model()
@@ -28,3 +30,16 @@ class DreamyUserChangeForm(UserChangeForm):
         """This class overrides the User model definition"""
 
         model = User
+
+
+class DreamySubmitPostForm(ModelForm):
+    """This class defines the form to submit images to Dreamy"""
+
+    author = ModelChoiceField(queryset=User.objects.all(), widget=HiddenInput())
+    date = DateTimeField(widget=HiddenInput())
+
+    class Meta:
+        """This class maps the form to the Post model definition"""
+
+        model = Post
+        fields = ['description', 'image']
